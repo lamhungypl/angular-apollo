b40b601ec845616dccaa36d01bd7f1aef24fc2e0
lamhung <lamhungypl@gmail.com>
2021-07-08 10:58:18 +0700

    feat (login): add antd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './core/providers/auth.guard';
 import { AuthLayoutComponent } from './layout/auth/auth.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent, AuthLayoutComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [AuthGuard],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [AuthGuard, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
